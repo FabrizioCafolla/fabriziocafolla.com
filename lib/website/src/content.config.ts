47548c6cf121a91346564a8bcd2aef37f4cd3d40
Fabrizio Cafolla <developer@fabriziocafolla.com>
2026-06-09 23:41:13 +0200

    feat: add guides and photos sections (#1)

--- lib/website/src/content.config.ts
+++ lib/website/src/content.config.ts
@@ -1,5 +1,6 @@
 import { glob } from "astro/loaders";
-import { defineCollection, z } from "astro:content";
+import { z } from "astro/zod";
+import { defineCollection } from "astro:content";
 
 const posts = defineCollection({
   // Load Markdown and MDX files in the `src/content/posts/` directory.
@@ -18,4 +19,42 @@ const posts = defineCollection({
   }),
 });
 
-export const collections = { posts };
+const guides = defineCollection({
+  loader: glob({ base: "./src/content/guides", pattern: "**/*.{yaml,yml}" }),
+  schema: z.object({
+    title: z.string(),
+    slug: z.string(),
+    aliases: z.array(z.string()).optional(),
+    description: z.string().optional(),
+    tips: z.string().optional(),
+    places: z.array(
+      z.object({
+        name: z.string(),
+        cost: z.enum(["low", "mid-low", "middle", "mid-high", "high"]).optional(),
+        maps: z.url().optional(),
+        notes: z.string().optional(),
+        hours: z.array(z.string()).optional(),
+        type: z.array(z.string()),
+        tags: z.array(z.string()).optional(),
+        rating: z.enum(["good", "recommended", "top", "unique"]),
+      })
+    ),
+  }),
+});
+
+const photos = defineCollection({
+  loader: glob({ base: "./src/content/photos", pattern: "**/*.{yaml,yml}" }),
+  schema: z.object({
+    photos: z.array(
+      z.object({
+        title: z.string(),
+        description: z.string(),
+        location: z.string(),
+        date: z.string(),
+        path: z.string(),
+      })
+    ),
+  }),
+});
+
+export const collections = { posts, guides, photos };
